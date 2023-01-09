8f0cfec3e763562f92febce08d2eb460fd304ee0
Rytecc <64328851+Rytecc@users.noreply.github.com>
2023-01-09 16:22:12 -0500

    Created Stuff for Lucas Soliman
    Lead Coder

---
+++ src/main/Lucas_Soliman/RobotDrive.java
@@ -0,0 +1,18 @@
+package Lucas_Soliman;
+
+public class RobotDrive {
+    private DifferentialDrive driveInstance;
+    private float currentSpeed;
+    
+    public RobotDrive(int topLeft, int bottomLeft, int topRight, int bottomRight) {
+        
+    }
+
+    public void TeleopTick() {
+
+    }
+
+    public void DriveRobot(double motorPower, double zRotation) {
+
+    }
+}
---
+++ src/main/Lucas_Soliman/Utility.java
@@ -0,0 +1,5 @@
+package Lucas_Soliman;
+
+public class Utility {
+    
+}
