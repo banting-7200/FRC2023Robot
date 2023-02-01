e327fab4af7500f74fc07104c01db1fb4fa35232
Rytecc <64328851+Rytecc@users.noreply.github.com>
2023-02-01 16:55:37 -0500

    Added in arduino I2C file.

---
+++ .arduinoprograms/PixyI2C/PixyI2C.ino
@@ -0,0 +1,28 @@
+#include <Pixy2.h>
+#include<Wire.h>
+
+int xToWrite;
+int directionToWrite;
+Pixy2 camera;
+
+void setup() {
+  Wire.begin(1);
+  camera.init();
+  camera.onRequest(onRequest);
+}
+
+void loop() {
+  camera.ccc.getBlocks();
+
+  double avgX = 0;
+  for(int i = 0; i < camera.ccc.numBlocks; i++) {
+    avgX += camera.ccc.blocks[i].m_x;
+  }
+
+  xToWrite = (int)(avgX / (double)camera.ccc.numBlocks);
+  directionToWrite = xToWrite < 0 ? -1 : 1;
+}
+
+void onRequest() {  
+  Wire.write(directionToWrite);
+}
