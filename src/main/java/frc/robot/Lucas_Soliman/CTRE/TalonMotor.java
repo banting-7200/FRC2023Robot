2dd417b1c70a48cf91519587501b7bab775a9749
Rytecc <64328851+Rytecc@users.noreply.github.com>
2023-02-02 17:42:55 -0500

    Commit CTRE Talon Motor class

---
+++ src/main/java/frc/robot/Lucas_Soliman/CTRE/TalonMotor.java
@@ -0,0 +1,38 @@
+package frc.robot.Lucas_Soliman.CTRE;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+/*
+ * Author: Lucas Soliman
+ * Date Created: February 2, 2023
+ * 
+ * This class' encapsulates a TalonFX controller.
+ * The main reason this class exists is due to
+ * the config functions that are available for
+ * - customizing
+ * - tweaking
+ * - calibrating
+ * 
+ * motor controllers.
+ */
+public class TalonMotor {
+    private TalonFX talonMotor;
+
+    public TalonMotor(int canID) {
+        talonMotor = new TalonFX(canID);
+        talonMotor.configFactoryDefault();
+        talonMotor.config_kF(0, 0);
+        talonMotor.config_kP(0, 0.1);
+        talonMotor.config_kI(0, 0.0025);
+        talonMotor.config_kD(0, 0);
+    }
+
+    public void set(ControlMode mode, double arg) {
+        talonMotor.set(mode, arg);
+    }
+
+    public TalonFX getMotor() {
+        return talonMotor;
+    }
+}
