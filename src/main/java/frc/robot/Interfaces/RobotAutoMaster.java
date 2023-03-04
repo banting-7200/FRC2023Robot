46e32d4d910fc236c910b1348ae3c05bf3f32b4a
Rytecc <ryanstechset@gmail.com>
2023-03-03 23:31:43 -0500

    More cleaning
    I should be sleeping, but screw it

--- src/main/java/frc/robot/Interfaces/RobotAutoMaster.java
+++ src/main/java/frc/robot/Interfaces/RobotAutoMaster.java
@@ -1,5 +1,9 @@
 package frc.robot.Interfaces;
 
+/*
+ * This should be a base class that can be inherited, not an interface.
+ * Using polymorphism will eliminate boilerplate code that needs to be written.
+ */
 public interface RobotAutoMaster {
     public void runAuto();
     public void resetAuto();
