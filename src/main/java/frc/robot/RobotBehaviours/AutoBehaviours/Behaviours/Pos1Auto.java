9089714cf22ff0554a5be001b57a316afd74ba16
JonaBMHS <jona5251@scdsb.on.ca>
2023-03-28 09:25:15 -0400

    awesome changes
    - changed creepspeed to 0.5 (was 0.4)
    - changed autodrive parameters

--- src/main/java/frc/robot/RobotBehaviours/AutoBehaviours/Behaviours/Pos1Auto.java
+++ src/main/java/frc/robot/RobotBehaviours/AutoBehaviours/Behaviours/Pos1Auto.java
@@ -48,16 +48,14 @@ public class Pos1Auto implements RobotAutoMaster{
             //Open and close claw
             new AutoClaw(true, 0.5),
             new AutoClaw(false, 0.5),
-            new AutoDrive(driverInstance, 3.5, 0.0, -0.4)
-
-            /*
+            new AutoDrive(driverInstance, 3.5, 0.0, -0.4),
+            
             //Move to pickup position and backup at the same time
             new AutoParallel(new RobotAutoBehaviour[] {
-                new AutoDrive(drive, 3.5, 0, -0.4)
+                new AutoDrive(driverInstance, 1, -0.5, 0),
                 new AutoLiftPos(lift, CoPilotControls.MACRO_PICKUP.get(), 0.6),
                 new AutoShoulderPos(shoulder, CoPilotControls.MACRO_PICKUP.get())
             })
-            */
         };
 
         autoPtr = 0;
